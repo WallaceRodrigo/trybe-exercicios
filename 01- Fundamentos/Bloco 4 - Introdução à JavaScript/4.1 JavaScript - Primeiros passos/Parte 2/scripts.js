dbfe6af56dd5016716177edc40224593f71c3407
Wallace <wallacerodrigo2004@gmail.com>
2022-09-21 15:29:50 -0300

    parte 2

---
+++ 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.1 JavaScript - Primeiros passos/Parte 2/scripts.js
@@ -0,0 +1,24 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+console.log(typeof patientEmail)
+
+//Crie uma constante chamada base e atribua a ela o valor 5.
+//Crie uma constante uma chamada height e atribua a ela o valor 8.
+//Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela height. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+//Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+
+const base = 5;
+const height = 8;
+const area = base * height;
+const perimeter = base + base + height + height;
+console.log(area, perimeter)
+
