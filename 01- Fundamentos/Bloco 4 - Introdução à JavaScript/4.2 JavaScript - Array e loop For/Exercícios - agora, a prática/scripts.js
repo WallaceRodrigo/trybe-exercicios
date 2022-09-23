a954dc6ca8fdc3a6c1d33915dfce8519853af50c
Wallace <wallacerodrigo2004@gmail.com>
2022-09-23 17:11:25 -0300

    4.2 continue

--- 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.2 JavaScript - Array e loop For/Exercícios - agora, a prática/scripts.js
+++ 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.2 JavaScript - Array e loop For/Exercícios - agora, a prática/scripts.js
@@ -1,48 +1,52 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27,];
-//let sum = 0;
+let sum = 0;
 
-//for (number of numbers) {
-//  console.log(number);
-//}
+for (number of numbers) {
+  console.log(number);
+}
 
-////
+//
 
-//for (let index = 0; index < numbers.length; index += 1) {
-//  sum += numbers[index]
-//}
-//console.log(sum);
+for (let index = 0; index < numbers.length; index += 1) {
+  sum += numbers[index]
+}
+console.log(sum);
 
-////
-//let average = sum / numbers.length
-//console.log(average)
+//
+let average = sum / numbers.length
+console.log(average)
 
-////
+//
 
-//if (average > 20) {
-//  console.log('valor maior que 20')
-//}else {
-//  console.log ('valor menor ou igual a 20')
-//}
+if (average > 20) {
+  console.log('valor maior que 20')
+}else {
+  console.log ('valor menor ou igual a 20')
+}
 
-////
+//
 
-//let maior = 0;
-//for (let index = 0; index < numbers.length; index += 1) {
-//   if ( numbers.sort()[index] > maior ) {
-//      maior = numbers[index];
-//      console.log(maior)
-//   }
-//}
+let higherNumber = numbers[0];
 
-//let menor = 10;
-//for (let index = 0; index < numbers.length; index += 1) {
-//   if ( numbers[index] < menor ) {
-//      menor = numbers[index];
-//      console.log(menor)
-//   }
-//}
+for (let index = 1; index < numbers.length; index += 1) {
+   if ( numbers[index] > higherNumber ) {
+			higherNumber = numbers[index];
+   }
+}
+console.log(higherNumber)
 
+//
 
+let smallerNumber = numbers[0];
+
+for (let index = 0; index < numbers.length; index += 1) {
+   if ( numbers[index] < smallerNumber ) {
+      smallerNumber = numbers[index];
+   }
+}
+console.log(smallerNumber)
+
+//
 
 for (let index = 0; index < numbers.length; index += 1) {
   if( numbers[index] % 2 === 0){
@@ -51,6 +55,3 @@ for (let index = 0; index < numbers.length; index += 1) {
   console.log('ÍMPAR');
   }
 }
-
-
-
