38b3a2856e9b0bcecaeeadc815a034f8e7051d60
Wallace <wallacerodrigo2004@gmail.com>
2022-09-23 18:40:34 -0300

    4.3 end

--- 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.3 JavaScript - Lógica de Programação e Algoritmos/Exercícios - agora, a prática/scripts.js
+++ 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.3 JavaScript - Lógica de Programação e Algoritmos/Exercícios - agora, a prática/scripts.js
@@ -17,3 +17,43 @@ for (let index = 0; index < word.length; index += 1) {
 }
 
 console.log(reverseWord);
+
+//
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let biggerWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+	if (array[index].length > biggerWord.length){
+		biggerWord = array[index];
+	}
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggerWord)
+console.log(smallestWord)
+
+//
+//Auxilio do Gabarito//
+
+let biggestPrimeNumber = 0;
+
+for (let indexN = 2; indexN <= 50; indexN += 1) {
+  let isPrime = true;
+  for (let indexDiv = 2; indexDiv < indexN; indexDiv += 1) {
+    if (indexN % indexDiv === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = indexN;
+  }
+}
+
+console.log(biggestPrimeNumber);
