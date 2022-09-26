ba1c3281341dfc96a220cc785f575b1732bb83f0
Wallace <wallacerodrigo2004@gmail.com>
2022-09-26 20:49:04 -0300

    4.4

--- 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.4 JavaScript - Objetos e funções/Exercícios - funções/scripts.js
+++ 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.4 JavaScript - Objetos e funções/Exercícios - funções/scripts.js
@@ -1,9 +1,35 @@
 function palindrome(word) {
   if (word.split('').reverse().join('') === word){
-    return true
+    return true;
   }else {
-    return false
-  }
+    return false;
+  };
 }
-console.log(palindrome('arara'))
+console.log(palindrome('arara'));
 
+//
+//AUXILIO DO GABARITO//
+
+function higher(numbers) {
+  let higherNumber = [0];
+  for (let key in numbers) {
+    if (numbers[higherNumber] < numbers[key]){
+      higherNumber = key;
+    }
+  }
+  return higherNumber
+} 
+
+console.log(higher([2, 3, 6, 7, 10, 1]));
+
+function smallest(numbers) {
+  let smallestNumber = [0];
+  for (let key in numbers) {
+    if (numbers[smallestNumber] > numbers[key]){
+      smallestNumber = key;
+    }
+  }
+  return smallestNumber
+} 
+
+console.log(smallest([2, 3, 6, 7, 10, 1]));
