97f520980ea5b4178f16713c5209acd8c1ce7d11
Wallace <wallacerodrigo2004@gmail.com>
2022-09-26 16:36:40 -0300

    4.4

---
+++ 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.4 JavaScript - Objetos e funções/Parte 3/scripts.js
@@ -0,0 +1,58 @@
+function math (a , b) {
+  function sum (a , b) {
+    return a + b;
+  }
+  
+  function minus (a , b) {
+    return a - b;
+  }
+
+  function multi (a , b) {
+    return a * b;
+  }
+
+  function div (a , b) {
+    return a / b;
+  }
+
+  function mod (a , b) {
+    return a % b;
+  }
+}
+console.log(math)
+
+//
+
+function higherNumber (a , b) {
+  if (a > b) {
+    return a + ' é maior que ' + b;
+  }else if (b > a) {
+    return b + ' é maior que ' + a;
+  }else {
+    return 'os dois números são iguais';
+  }
+}
+console.log(higherNumber(1, 3))
+
+//
+
+function higherNumber2 (a , b, c) {
+  if (a > b && a > c) {
+    return a + ' é maior';
+  }else if (b > a && b > c) {
+    return b + ' é maior';
+  }else {
+    return c + ' é maior';
+  }
+}
+console.log(higherNumber2(5, 3, 2))
+
+//
+
+let vari = true
+
+function isPositive (vari) {
+  if (vari === true) {
+
+  }
+}
