d9d189b3afb5536c13664550fd8ed00fbdab129c
Wallace <wallacerodrigo2004@gmail.com>
2022-09-27 13:54:54 -0300

    mentoria

--- 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.4 JavaScript - Objetos e funções/Mentoria function gus/scripts.js
+++ 01- Fundamentos/Bloco 4 - Introdução à JavaScript/4.4 JavaScript - Objetos e funções/Mentoria function gus/scripts.js
@@ -8,3 +8,25 @@ function sumFunction(array){
 }
 
 console.log(sumFunction([1, 2, 3, 4, 5]))
+
+
+function nbaChampion(lakers, celtics){
+  let countLakers = 0;
+  let countCeltics = 0;
+
+  for(let index = 0; index < lakers.length; index += 1){
+    if(lakers[index] > celtics[index]){
+      countLakers += 1;
+    } else {
+      countCeltics += 1;
+    }
+  }
+
+  if(countLakers > countCeltics){
+    return 'Lakers foi campeão com ' + countLakers + ' vitórias!';
+  }else {
+    return 'Celtics foi campeão com ' + countCeltics + ' vitórias!';
+  }
+}
+
+console.log(nbaChampion([106, 98, 107], [108, 99, 110]))
