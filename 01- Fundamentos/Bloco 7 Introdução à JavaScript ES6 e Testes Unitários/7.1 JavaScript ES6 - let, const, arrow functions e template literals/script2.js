dcd4c52be552fdcd1d6d496ebcafbea4110c3c03
Wallace <wallacerodrigo2004@gmail.com>
2022-10-17 18:22:06 -0300

    7.1 exer part2

--- 01- Fundamentos/Bloco 7 Introdução à JavaScript ES6 e Testes Unitários/7.1 JavaScript ES6 - let, const, arrow functions e template literals/script2.js
+++ 01- Fundamentos/Bloco 7 Introdução à JavaScript ES6 e Testes Unitários/7.1 JavaScript ES6 - let, const, arrow functions e template literals/script2.js
@@ -1,1 +1,30 @@
-const factorial 
+const factorial = (number) => {
+  let result = 1;
+  for (let index = 2; index <= number; index += 1) {
+    result *= index
+  }
+
+  return result
+}
+
+console.log(`O fatorial é ${factorial(5)}`);
+
+//RESOLVIDO COM AUXILIO DO GABARITO//
+
+const longestWord = (text) => {
+  const wordArray = text.split(' ');
+  let maxLength = 0;
+  let result = '';
+
+  for (const word of wordArray) {
+      if (word.length > maxLength) {
+          maxLength = word.length;
+          result = word;
+      }
+  }
+
+  return result;
+}
+
+console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+
