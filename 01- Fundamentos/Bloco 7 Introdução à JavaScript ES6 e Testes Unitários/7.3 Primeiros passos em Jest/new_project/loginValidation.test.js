cec70bd5ef911bd966b32a443b0249db35dbc16f
Wallace <wallacerodrigo2004@gmail.com>
2022-10-19 16:34:56 -0300

    jest initial2

--- 01- Fundamentos/Bloco 7 Introdução à JavaScript ES6 e Testes Unitários/7.3 Primeiros passos em Jest/new_project/loginValidation.test.js
+++ 01- Fundamentos/Bloco 7 Introdução à JavaScript ES6 e Testes Unitários/7.3 Primeiros passos em Jest/new_project/loginValidation.test.js
@@ -6,20 +6,6 @@ const {
 } = require("./loginValidation.js");
 
 describe("Testa a função verifyCredentials()", () => {
-  
-  it("verifyCredentials() chama a função correta dependendo do valor de userName e password", () => {
-    
-    const user = {
-      userName: 'Bob',
-      password: 123456,
-    };
-      
-    const { userName, password } = user;
-
-    expect(verifyCredentials({ userName, password })).toBe(
-      "Hello, Joana! Que bom ter você de volta"
-    ); 
-  });
 
   it("greetingMessage() retorna uma mensagem no formato: `Hello, ${user}! Que bom ter você de volta`", () => {
     expect(greetingMessage("Lucas")).toBe(
