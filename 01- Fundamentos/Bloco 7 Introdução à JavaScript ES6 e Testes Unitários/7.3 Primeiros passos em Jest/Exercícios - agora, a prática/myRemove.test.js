0a8233b28c2c0c744980814f5fe040eccb821c9d
Wallace <wallacerodrigo2004@gmail.com>
2022-10-19 18:58:24 -0300

    7.3 exer 3

--- 01- Fundamentos/Bloco 7 Introdução à JavaScript ES6 e Testes Unitários/7.3 Primeiros passos em Jest/Exercícios - agora, a prática/myRemove.test.js
+++ 01- Fundamentos/Bloco 7 Introdução à JavaScript ES6 e Testes Unitários/7.3 Primeiros passos em Jest/Exercícios - agora, a prática/myRemove.test.js
@@ -1,6 +1,6 @@
 const myRemove = require("./myRemove");
 
-describe('Test function myRemove', () => {
+describe('Test myRemove function', () => {
 
   test('Return [1, 2, 4] when myRemove([1, 2, 3, 4], 3)', () => {
     expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
