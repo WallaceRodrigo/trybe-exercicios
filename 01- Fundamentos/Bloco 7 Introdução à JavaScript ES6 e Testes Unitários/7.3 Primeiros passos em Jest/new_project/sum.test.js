98d3d397395634bae934a73a934c3810c5a5c79c
Wallace <wallacerodrigo2004@gmail.com>
2022-10-19 16:36:08 -0300

    jest initial3

--- 01- Fundamentos/Bloco 7 Introdução à JavaScript ES6 e Testes Unitários/7.3 Primeiros passos em Jest/new_project/sum.test.js
+++ 01- Fundamentos/Bloco 7 Introdução à JavaScript ES6 e Testes Unitários/7.3 Primeiros passos em Jest/new_project/sum.test.js
@@ -1,8 +1,8 @@
-const sum = (a, b) => a + b;
+//const sum = (a, b) => a + b;
 
-test('soma dois valores', () => {
-  expect(sum(2, 3)).toEqual(5);
-});
-test('soma dois valores2', () => {
-  expect(sum(5, 5)).toEqual(10);
-});
+//test('soma dois valores', () => {
+//  expect(sum(2, 3)).toEqual(5);
+//});
+//test('soma dois valores2', () => {
+//  expect(sum(5, 5)).toEqual(10);
+//});
