ebccd4c8a08e192e8fea2c3ae5c037692c38d951
Wallace <wallacerodrigo2004@gmail.com>
2022-10-25 17:22:50 -0300

    8.2 part3

--- 01- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/Parte 1/.someAnd.every.js
+++ 01- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/8.2 - JavaScript ES6 - Higher Order Functions - forEach, find, some, every, sort/Parte 1/.someAnd.every.js
@@ -1,7 +1,19 @@
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
-const hasName = (arr, name) => {
-  //Adicione seu código aqui
-};
+const hasName = (arr, name) => arr.some((inputName) => inputName === name)
 
-console.log(hasName(names, 'Ana'));
+console.log(hasName(names, 'José'));
+
+////////////////////////////////////
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 18 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => arr.every((person) => person.age >= minimumAge)
+
+console.log(verifyAges(people, 18));
