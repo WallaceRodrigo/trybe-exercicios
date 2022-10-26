84cc2d3dba441c5649f65eb1adb4d6f0be48326f
Wallace <wallacerodrigo2004@gmail.com>
2022-10-26 15:36:16 -0300

    8.3 exer part1

--- 01- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/8.3 JavaScript ES6 - Higher Order Functions - map e filter/Parte 1/scripts.js
+++ 01- Fundamentos/Bloco 8 - Higher Order Functions do JavaScript ES6/8.3 JavaScript ES6 - Higher Order Functions - map e filter/Parte 1/scripts.js
@@ -1,2 +1,9 @@
-const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
 const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
