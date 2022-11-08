9695fff3644f1e7ea8b153f8c3e684f548314e51
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2022-11-08 19:44:13 -0300

    front end day 1

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "trybe-frontend",
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
