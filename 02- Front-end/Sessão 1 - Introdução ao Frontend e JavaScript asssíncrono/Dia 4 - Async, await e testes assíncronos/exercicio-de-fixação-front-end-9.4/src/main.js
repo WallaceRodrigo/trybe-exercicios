056cdc799aa00b54593467ca1cbbf8997f0daccd
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2022-11-11 16:06:13 -0300

    exercises 9.3 & 9.4

---
+++ 02- Front-end/Sessão 1 - Introdução ao Frontend e JavaScript asssíncrono/Dia 4 - Async, await e testes assíncronos/exercicio-de-fixação-front-end-9.4/src/main.js
@@ -0,0 +1,38 @@
+import Swal from 'sweetalert2';
+
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const preEl = document.querySelector('pre');
+
+async function handleClick() {
+  const cep = inputEl.value;
+
+  try {
+    const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await result.json();
+    preEl.innerHTML = '';
+
+    const obj = Object.entries(data);
+    obj.map((ob) => {
+      preEl.innerHTML += `
+      ${ob[0]}: ${ob[1]}
+      `;
+      return '';
+    });
+
+    if (data.erro === true) {
+      preEl.innerHTML = 'CEP inválido!';
+      throw new Error('');
+    }
+
+    return data;
+  } catch (error) {
+    return Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Você precisa passar um CEP válido',
+    });
+  }
+}
+
+buttonEl.addEventListener('click', handleClick);
