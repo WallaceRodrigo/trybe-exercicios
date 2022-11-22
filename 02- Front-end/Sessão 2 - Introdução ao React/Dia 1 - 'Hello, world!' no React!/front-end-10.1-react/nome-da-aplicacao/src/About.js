4a0b327c7fd4d39ed0c2573ce0157b5c47a1645f
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2022-11-22 16:00:55 -0300

    fixação react

---
+++ 02- Front-end/Sessão 2 - Introdução ao React/Dia 1 - 'Hello, world!' no React!/front-end-10.1-react/nome-da-aplicacao/src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Wallace Rodrigo</h1>
+        <p>Gosto de tecnologia e jogos</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Htlm</li>
+          <li>Css</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
