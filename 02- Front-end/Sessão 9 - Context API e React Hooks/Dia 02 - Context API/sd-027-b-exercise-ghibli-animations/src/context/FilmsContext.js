9823d0103045ee10585582dfa35a0b50c4267281
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-02-17 13:21:40 -0300

    frontend 9.2

---
+++ 02- Front-end/Sessão 9 - Context API e React Hooks/Dia 02 - Context API/sd-027-b-exercise-ghibli-animations/src/context/FilmsContext.js
@@ -0,0 +1,7 @@
+// implemente a estrutura do context aqui.
+// src/context/FilmContext.js
+import { createContext } from 'react';
+
+const FilmsContext = createContext();
+
+export default FilmsContext;
