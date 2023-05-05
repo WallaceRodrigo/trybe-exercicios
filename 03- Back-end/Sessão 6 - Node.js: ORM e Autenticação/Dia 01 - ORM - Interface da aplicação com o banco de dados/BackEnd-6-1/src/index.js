9f026cccefb138a46ab924f7a7c2da9c1b7ea325
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-05-05 12:47:40 -0300

    exer 1 - 8 backend 6.1

---
+++ 03- Back-end/Sessão 6 - Node.js: ORM e Autenticação/Dia 01 - ORM - Interface da aplicação com o banco de dados/BackEnd-6-1/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const BooksController = require('./controllers/BooksController');
+
+app.use(express.json());
+
+app.get('/books', BooksController.getAll);
+
+app.get("/books/:id", BooksController.getById);
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
---
+++ 03- Back-end/Sessão 6 - Node.js: ORM e Autenticação/Dia 01 - ORM - Interface da aplicação com o banco de dados/BackEnd-6-1/src/migrations/20230505153153-create-books.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, DataTypes) {
+    return queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      pageQuantity: {
+        allowNull: true,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+
+  async down (queryInterface) {
+    return queryInterface.dropTable('Books');
+  },
+};
