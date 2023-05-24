ffb2fb1cbee9ed4f4d93aa770ebf3b2d06ba1cb8
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-05-24 13:06:51 -0300

    BackEnd 8.3

---
+++ 03- Back-end/Sessão 8 - Introdução a TypeScript/Dia 03 - API REST com Express, JWT, Sequelize e TypeScript/sd-027-b-exercise-trybe-travel/src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+import packagesRouter from './routers/package.router';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.status(200).send('Aplicação está funcionando!');
+});
+
+app.use(packagesRouter);
+
+export default app;
